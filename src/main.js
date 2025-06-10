cf0038c6b36525896780f8ba50abc703e7bec9e0
GMO_517 <ra89517r@gmail.com>
2025-06-10 11:22:56 +0800

    chore: element-plus pinia插件匯入 建置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
 
-import './css/style.css'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import "./css/style.css";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+import { createPinia } from "pinia";
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+
+const pinia = createPinia();
+app.use(pinia);
+
+app.mount("#app");
