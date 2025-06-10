7e01258c99ca0f150dbb8e40cda255e2dea92bfc
GMO_517 <ra89517r@gmail.com>
2025-06-10 11:19:13 +0800

    feat: 新增跨域請求代理

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    "process.env": process.env,
   },
   plugins: [vue()],
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  }
-})
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.coingecko.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
