130d4fd95524f4ea615d3036aec00d976bf5a422
GMO_517 <ra89517r@gmail.com>
2025-06-17 10:43:04 +0800

    fix: 補上數字轉換

--- src/charts/BarChart01.vue
+++ src/charts/BarChart01.vue
@@ -8,35 +8,53 @@
 </template>
 
 <script>
-import { ref, watch, onMounted, onUnmounted } from 'vue'
-import { useDark } from '@vueuse/core'
-import { getChartColors } from './ChartjsConfig'
+import { ref, watch, onMounted, onUnmounted } from "vue";
+import { useDark } from "@vueuse/core";
+import { getChartColors } from "./ChartjsConfig";
 
 import {
-  Chart, BarController, BarElement, LinearScale, TimeScale, Tooltip, Legend,
-} from 'chart.js'
-import 'chartjs-adapter-moment'
+  Chart,
+  BarController,
+  BarElement,
+  LinearScale,
+  TimeScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import "chartjs-adapter-moment";
 
 // Import utilities
 import { formatTWNumber } from "../utils/Utils";
 
-Chart.register(BarController, BarElement, LinearScale, TimeScale, Tooltip, Legend)
+Chart.register(
+  BarController,
+  BarElement,
+  LinearScale,
+  TimeScale,
+  Tooltip,
+  Legend
+);
 
 export default {
-  name: 'BarChart01',
-  props: ['data', 'width', 'height'],
+  name: "BarChart01",
+  props: ["data", "width", "height"],
   setup(props) {
+    const canvas = ref(null);
+    const legend = ref(null);
+    let chart = null;
+    const darkMode = useDark();
+    const {
+      textColor,
+      gridColor,
+      tooltipBodyColor,
+      tooltipBgColor,
+      tooltipBorderColor,
+    } = getChartColors();
 
-    const canvas = ref(null)
-    const legend = ref(null)
-    let chart = null
-    const darkMode = useDark()
-    const { textColor, gridColor, tooltipBodyColor, tooltipBgColor, tooltipBorderColor } = getChartColors()
-    
     onMounted(() => {
-      const ctx = canvas.value
+      const ctx = canvas.value;
       chart = new Chart(ctx, {
-        type: 'bar',
+        type: "bar",
         data: props.data,
         options: {
           layout: {
@@ -59,15 +77,15 @@ export default {
               },
               grid: {
                 color: darkMode.value ? gridColor.dark : gridColor.light,
-              },              
+              },
             },
             x: {
               type: "category",
               time: {
-                parser: 'MM-DD-YYYY',
-                unit: 'month',
+                parser: "MM-DD-YYYY",
+                unit: "month",
                 displayFormats: {
-                  month: 'MMM YY',
+                  month: "MMM YY",
                 },
               },
               border: {
@@ -90,14 +108,20 @@ export default {
                 title: () => false, // Disable tooltip title
                 label: (context) => formatTWNumber(context.parsed.y),
               },
-              bodyColor: darkMode.value ? tooltipBodyColor.dark : tooltipBodyColor.light,
-              backgroundColor: darkMode.value ? tooltipBgColor.dark : tooltipBgColor.light,
-              borderColor: darkMode.value ? tooltipBorderColor.dark : tooltipBorderColor.light,
+              bodyColor: darkMode.value
+                ? tooltipBodyColor.dark
+                : tooltipBodyColor.light,
+              backgroundColor: darkMode.value
+                ? tooltipBgColor.dark
+                : tooltipBgColor.light,
+              borderColor: darkMode.value
+                ? tooltipBorderColor.dark
+                : tooltipBorderColor.light,
             },
           },
           interaction: {
             intersect: false,
-            mode: 'nearest',
+            mode: "nearest",
           },
           animation: {
             duration: 500,
@@ -105,69 +129,79 @@ export default {
           maintainAspectRatio: false,
           resizeDelay: 200,
         },
-        plugins: [{
-          id: 'htmlLegend',
-          afterUpdate(c, args, options) {
-            const ul = legend.value
-            if (!ul) return
-            // Remove old legend items
-            while (ul.firstChild) {
-              ul.firstChild.remove()
-            }
-            // Reuse the built-in legendItems generator
-            const items = c.options.plugins.legend.labels.generateLabels(c)
-            items.forEach((item) => {
-              const li = document.createElement('li')
-              // Button element
-              const button = document.createElement('button')
-              button.style.display = 'inline-flex'
-              button.style.alignItems = 'center'
-              button.style.opacity = item.hidden ? '.3' : ''
-              button.onclick = () => {
-                c.setDatasetVisibility(item.datasetIndex, !c.isDatasetVisible(item.datasetIndex))
-                c.update()
+        plugins: [
+          {
+            id: "htmlLegend",
+            afterUpdate(c, args, options) {
+              const ul = legend.value;
+              if (!ul) return;
+              // Remove old legend items
+              while (ul.firstChild) {
+                ul.firstChild.remove();
               }
-              // Color box
-              const box = document.createElement('span')
-              box.style.display = 'block'
-              box.style.width = '12px'
-              box.style.height = '12px'
-              box.style.borderRadius = 'calc(infinity * 1px)'
-              box.style.marginRight = '8px'
-              box.style.borderWidth = '3px'
-              box.style.borderColor = item.fillStyle
-              box.style.pointerEvents = 'none'
-              // Label
-              const labelContainer = document.createElement('span')
-              labelContainer.style.display = 'flex'
-              labelContainer.style.alignItems = 'center'
-              const value = document.createElement('span')
-              value.classList.add('text-gray-800', 'dark:text-gray-100')
-              value.style.fontSize = '30px'
-              value.style.lineHeight = 'calc(2.25 / 1.875)'
-              value.style.fontWeight = '700'
-              value.style.marginRight = '8px'
-              value.style.pointerEvents = 'none'
-              const label = document.createElement('span')
-              label.classList.add('text-gray-500', 'dark:text-gray-400')
-              label.style.fontSize = '14px'
-              label.style.lineHeight = 'calc(1.25 / 0.875)'
-              const theValue = c.data.datasets[item.datasetIndex].data.reduce((a, b) => a + b, 0)
-              const valueText = document.createTextNode(formatValue(theValue))
-              const labelText = document.createTextNode(item.text)
-              value.appendChild(valueText)
-              label.appendChild(labelText)
-              li.appendChild(button)
-              button.appendChild(box)
-              button.appendChild(labelContainer)
-              labelContainer.appendChild(value)
-              labelContainer.appendChild(label)
-              ul.appendChild(li)
-            })
+              // Reuse the built-in legendItems generator
+              const items = c.options.plugins.legend.labels.generateLabels(c);
+              items.forEach((item) => {
+                const li = document.createElement("li");
+                // Button element
+                const button = document.createElement("button");
+                button.style.display = "inline-flex";
+                button.style.alignItems = "center";
+                button.style.opacity = item.hidden ? ".3" : "";
+                button.onclick = () => {
+                  c.setDatasetVisibility(
+                    item.datasetIndex,
+                    !c.isDatasetVisible(item.datasetIndex)
+                  );
+                  c.update();
+                };
+                // Color box
+                const box = document.createElement("span");
+                box.style.display = "block";
+                box.style.width = "12px";
+                box.style.height = "12px";
+                box.style.borderRadius = "calc(infinity * 1px)";
+                box.style.marginRight = "8px";
+                box.style.borderWidth = "3px";
+                box.style.borderColor = item.fillStyle;
+                box.style.pointerEvents = "none";
+                // Label
+                const labelContainer = document.createElement("span");
+                labelContainer.style.display = "flex";
+                labelContainer.style.alignItems = "center";
+                const value = document.createElement("span");
+                value.classList.add("text-gray-800", "dark:text-gray-100");
+                value.style.fontSize = "30px";
+                value.style.lineHeight = "calc(2.25 / 1.875)";
+                value.style.fontWeight = "700";
+                value.style.marginRight = "8px";
+                value.style.pointerEvents = "none";
+                const label = document.createElement("span");
+                label.classList.add("text-gray-500", "dark:text-gray-400");
+                label.style.fontSize = "14px";
+                label.style.lineHeight = "calc(1.25 / 0.875)";
+                const theValue = c.data.datasets[item.datasetIndex].data.reduce(
+                  (a, b) => a + b,
+                  0
+                );
+                const valueText = document.createTextNode(
+                  formatTWNumber(theValue)
+                );
+                const labelText = document.createTextNode(item.text);
+                value.appendChild(valueText);
+                label.appendChild(labelText);
+                li.appendChild(button);
+                button.appendChild(box);
+                button.appendChild(labelContainer);
+                labelContainer.appendChild(value);
+                labelContainer.appendChild(label);
+                ul.appendChild(li);
+              });
+            },
           },
-        }],
-      })
-    })
+        ],
+      });
+    });
     // 監聽外部 data 變動，手動更新 chart
     watch(
       () => props.data,
@@ -180,33 +214,34 @@ export default {
       { deep: true }
     );
 
-    onUnmounted(() => chart.destroy())
+    onUnmounted(() => chart.destroy());
 
     watch(
       () => darkMode.value,
       () => {
         if (darkMode.value) {
-          chart.options.scales.x.ticks.color = textColor.dark
-          chart.options.scales.y.ticks.color = textColor.dark
-          chart.options.scales.y.grid.color = gridColor.dark
-          chart.options.plugins.tooltip.bodyColor = tooltipBodyColor.dark
-          chart.options.plugins.tooltip.backgroundColor = tooltipBgColor.dark
-          chart.options.plugins.tooltip.borderColor = tooltipBorderColor.dark
+          chart.options.scales.x.ticks.color = textColor.dark;
+          chart.options.scales.y.ticks.color = textColor.dark;
+          chart.options.scales.y.grid.color = gridColor.dark;
+          chart.options.plugins.tooltip.bodyColor = tooltipBodyColor.dark;
+          chart.options.plugins.tooltip.backgroundColor = tooltipBgColor.dark;
+          chart.options.plugins.tooltip.borderColor = tooltipBorderColor.dark;
         } else {
-          chart.options.scales.x.ticks.color = textColor.light
-          chart.options.scales.y.ticks.color = textColor.light
-          chart.options.scales.y.grid.color = gridColor.light
-          chart.options.plugins.tooltip.bodyColor = tooltipBodyColor.light
-          chart.options.plugins.tooltip.backgroundColor = tooltipBgColor.light
-          chart.options.plugins.tooltip.borderColor = tooltipBorderColor.light
+          chart.options.scales.x.ticks.color = textColor.light;
+          chart.options.scales.y.ticks.color = textColor.light;
+          chart.options.scales.y.grid.color = gridColor.light;
+          chart.options.plugins.tooltip.bodyColor = tooltipBodyColor.light;
+          chart.options.plugins.tooltip.backgroundColor = tooltipBgColor.light;
+          chart.options.plugins.tooltip.borderColor = tooltipBorderColor.light;
         }
-        chart.update('none')
-      })    
+        chart.update("none");
+      }
+    );
 
     return {
       canvas,
       legend,
-    }
-  }
-}
+    };
+  },
+};
 </script>
